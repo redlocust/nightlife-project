1fb48b919b355e7384dafe20559c8d70afe0d3b8
redlocust <redlocust1@gmail.com>
2016-12-28 12:54:18 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const morgan = require("morgan");
+const jade = require("jade");
 
+var app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello");
+});
+
+app.listen(3000);
+console.log("Server start on http://localhost:3000");
