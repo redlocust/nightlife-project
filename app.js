9415463918f68373c17a44ee5476a5af642226ff
redlocust <redlocust1@gmail.com>
2017-01-09 17:31:52 +0300

    Add list of items, image and description for items

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const morgan = require("morgan");
 const jade = require("jade");
 const path = require('path');
 const bodyParser = require('body-parser');
-const request = require('request');
-const Yelp = require('yelp');
+const home = require('./routes/home');
 
 var app = express();
 
-
-//app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.set("view engine", "jade");
@@ -18,34 +16,7 @@ app.set("view engine", "jade");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
-  res.render("index");
-});
-
-app.post("/", function (req, res) {
-
-  var yelp = new Yelp({
-    consumer_key: 'ypVfjVlyLK3qKsDVVe7BGQ',
-    consumer_secret: 'Q1t7GtOAeCbaHmUiOupOmI2THRA',
-    token: 'RNEVpmCnpTkwvCdEELJcJCBgb-MkvWgB',
-    token_secret: 'BgI4RPYjIKu3TwsI2SuG0zh2A98'
-  });
-
-
-  var userLocation = req.body.address;
-
-  yelp.search({term: 'food', location: userLocation})
-    .then(function (data) {
-      //res.send(data);
-      var itemList = data.businesses;
-
-      res.render("list", {itemList : itemList});
-    })
-    .catch(function (err) {
-      console.error(err);
-    });
-
-});
+app.use('/', home);
 
 app.listen(3000);
 console.log("Server start on http://localhost:3000");
---
+++ routes/home.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const Yelp = require('yelp');
+var router = express.Router();
+
+
+router.get("/", function (req, res) {
+  res.render("index");
+});
+
+
+router.post("/", function (req, res) {
+
+  var yelp = new Yelp({
+    consumer_key: 'ypVfjVlyLK3qKsDVVe7BGQ',
+    consumer_secret: 'Q1t7GtOAeCbaHmUiOupOmI2THRA',
+    token: 'RNEVpmCnpTkwvCdEELJcJCBgb-MkvWgB',
+    token_secret: 'BgI4RPYjIKu3TwsI2SuG0zh2A98'
+  });
+
+
+  var userLocation = req.body.address;
+
+  yelp.search({term: 'food', location: userLocation})
+    .then(function (data) {
+      //res.send(data);
+      var itemList = data.businesses;
+      res.render("list", {itemList});
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+
+});
+
+module.exports = router;
